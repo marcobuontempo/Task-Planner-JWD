4462884872584ea2ec1ab3cfb16f96f1d3b7912a
marcobuontempo <marco.buon36@gmail.com>
2021-09-27 11:39:20 +1000

    Fix clock format in footer

--- js/interactivity.js
+++ js/interactivity.js
@@ -40,17 +40,45 @@
 // FOOTER
     // Date/Time
     function display_ct6() {
-        var x = new Date()
-        var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
+        let x = new Date()
+        
+        date = x.getDate();
+        if (date<10) {
+            date = `0${date}`
+        }
+        
+        month = x.getMonth() + 1;
+            if (month<10) {
+                month = `0${month}`
+            }
+
+        year = x.getFullYear();
+
+        let ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
         hours = x.getHours( ) % 12;
         hours = hours ? hours : 12;
-        var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear(); 
-        x1 = x1 + " - " +  hours + ":" +  x.getMinutes() + ":" +  x.getSeconds() + ampm;
+            if (hours<10) {
+                hours = `0${hours}`
+            }
+
+        minutes = x.getMinutes();
+        if (minutes<10) {
+            minutes = `0${minutes}`
+        }
+
+        seconds = x.getSeconds();
+        if (seconds<10) {
+            seconds = `0${seconds}`
+        }
+
+
+
+        let x1 = `${date}/${month}/${year} - ${hours}:${minutes}:${seconds} ${ampm}`
         document.getElementById('ct6').innerHTML = x1;
         display_c6();
         }
         function display_c6(){
-        var refresh=1000; // Refresh rate in milli seconds
+        let refresh=1000; // Refresh rate in milli seconds
         mytime=setTimeout('display_ct6()',refresh)
         }
         display_c6(); 
