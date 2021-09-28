b43b77e2d8ce45f3b897d9e5177a0fbfe69e0248
marcobuontempo <marco.buon36@gmail.com>
2021-09-28 18:06:45 +1000

    Change CSS and createTask() function to hide Done Button unless status is in-progress

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -7,7 +7,7 @@ function createTask(title, description, assignedTo, dueDate, status, id) {
                 <h1>${title}</h1>
                 <p class="card-desc">${description}</p>
                 <p class="card-status status-${status}">${status.toUpperCase()}</p>
-                <img src="images/checkmark.png" alt="Done Button" class="done-button">
+                <img src="images/checkmark.png" alt="Done Button" class="done-button button-clicked-${status}">
             </div>
             <div class="card-footer footer-${status}">
                 <span class="card-due-date">${dueDate}</span>
